415ff2abfcc4fb4a694034f8c15f3e4adb991543
Rodrigo <rodrigo.o.2012.r@gmail.com>
2023-03-03 22:41:54 -0300

    Probando el proyecto

---
+++ src/components/notepad.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import ".././styles/notepad.css"
+
+const Notepad = () => {
+
+    return (
+        <div className="container">
+            <p>test</p>
+        </div>
+    )
+}
+
+export default Notepad;
